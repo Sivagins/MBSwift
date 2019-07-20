5df94f44036f0ead7429b658f94173a802376a9b
SiarheiSivahin <sivagins@gmail.com>
2019-07-20 14:01:44 +0300

    Adding pod project

---
+++ MBSwift/MBSwift/MBSwift.h
@@ -0,0 +1,19 @@
+//
+//  MBSwift.h
+//  MBSwift
+//
+//  Created by Siarhei on 7/20/19.
+//  Copyright © 2019 MBicycle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBSwift.
+FOUNDATION_EXPORT double MBSwiftVersionNumber;
+
+//! Project version string for MBSwift.
+FOUNDATION_EXPORT const unsigned char MBSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBSwift/PublicHeader.h>
+
+
